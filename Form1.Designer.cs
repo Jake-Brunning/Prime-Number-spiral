3ff30ce79e290dfcdf99ac6c61523152538b9733
BrunMan <87078099+Tarmacshrimp@users.noreply.github.com>
2022-05-01 17:49:31 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Prime_Number_spiral
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1397, 590);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1397, 558);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(1397, 540);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(1397, 525);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(1397, 510);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1601, 679);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prime_Number_spiral
+{
+    public partial class Form1 : Form
+    {
+        List<int> PrimeNumbers = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            SolidBrush brush = new SolidBrush(Color.White);
+            Graphics graphics = this.CreateGraphics();
+            Point DrawPoint = new Point(700, 500); //point to draw on
+            int NumOn = 1;
+            int HowManyToMake = 1;
+            for (double i = 0; i < 2000; i++) //i defines what direction the spiral is going    
+            {
+                if (i % 4 == 0)
+                {
+                    for (int h = 1; h <= HowManyToMake; h++)
+                    {
+                        NumOn += 1;
+                        DrawPoint.X += 1;
+                        if(Prime(NumOn) == true)
+                        {
+                            graphics.FillRectangle(brush, DrawPoint.X, DrawPoint.Y, 1, 1);
+                        }          
+                    }
+                }
+                else if (i % 4 == 1)
+                {
+                    for (int h = 1; h <= HowManyToMake; h++)
+                    {
+                        NumOn += 1;
+                        DrawPoint.Y += 1;
+                        if (Prime(NumOn) == true)
+                        {
+                            graphics.FillRectangle(brush, DrawPoint.X, DrawPoint.Y, 1, 1);
+                        }
+                    }
+                }
+                else if (i % 4 == 2)
+                {
+                    for (int h = 1; h <= HowManyToMake; h++)
+                    {
+                        NumOn += 1;
+                        DrawPoint.X -= 1;
+                        if (Prime(NumOn) == true)
+                        {
+                            graphics.FillRectangle(brush, DrawPoint.X, DrawPoint.Y, 1, 1);
+                        }
+                    }
+                }
+                else if (i % 4 == 3)
+                {
+                    for (int h = 1; h <= HowManyToMake; h++)
+                    {
+                        NumOn += 1;
+                        DrawPoint.Y -= 1;
+                        if (Prime(NumOn) == true)
+                        {
+                            graphics.FillRectangle(brush, DrawPoint.X, DrawPoint.Y, 1, 1);
+                        }
+                    }
+                } 
+                if(i % 2 == 0)
+                {
+                    HowManyToMake++;
+                }
+               
+            }
+            label1.Text = "Numbers Made: " + NumOn.ToString();
+            label2.Text = "Prime Number Count: " + PrimeNumbers.Count.ToString();
+            label3.Text = "Prime Number Percent " + ((double)PrimeNumbers.Count / (double)NumOn) * 100 + "%";
+        }
+        private bool Prime(int num)
+        {
+            for(int i = 2; i < num / 2; i++)
+            {
+                if(num % i == 0)
+                {
+                    return false;
+                }
+            }
+            PrimeNumbers.Add(num);
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(PrimeNumbers.ToString());
+        }
+    }
+}
